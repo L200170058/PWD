f17a5fe74978bf4f3fe97df3f60050ec3f0f639d
Sidiq Zainudin <43442447+L200170058@users.noreply.github.com>
2019-11-27 22:41:39 +0700

    Add files via upload

---
+++ Modul 09/admin.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Admin - Tugas</title>
+    <link rel="stylesheet" href="styleku.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center><h1>Haloo Admin</h1>
+    Silahkan logout dengan klik link <a href="logout2.php" class="link"><font color="blue">Disini</font></a></center>
+</body>
+</html>
---
+++ Modul 09/member.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Selamat Datang Member</title>
+    <link rel="stylesheet" href="styleku.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center><h1>Halooo Member</h1>
+    Silahkan logout dengan klik link <a href="logout2.php" class="link"><font color="blue">Disini</font></a></center>
+</body>
+</html>
